54cecae14648597e28d640497f457e89c745e425
ilyashulga <shulga.ilya@gmail.com>
2022-11-19 12:33:49 +0200

    Relative session folder +(global host)

--- app.py
+++ app.py
@@ -547,7 +547,7 @@ def delete_item():
 
 
 if __name__ == '__main__':
-    #app.run(host="0.0.0.0")
-    app.run(debug=True)
+    app.run(host="0.0.0.0")
+    #app.run(debug=True)
 
     #app.run(host="0.0.0.0", debug=True)
